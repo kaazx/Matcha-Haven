ccd84d45044dfc29bfaf7c13b6f80575818862e3
Kirsten <keithleenzapanta@gmail.com>
2025-10-20 22:29:02 +0800

    20th commit: Made changes across all files. Finalized

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+    overlay.classList.remove("active");
+    document.body.classList.remove("no-scroll");
+}));
